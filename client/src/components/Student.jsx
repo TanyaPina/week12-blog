aa9240ac782756186381eeb21e7c8316a25ddf1e
Tanya Pina <tanyapina15@gmail.com>
2023-04-05 16:35:16 -0700

    formated edit button

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -18,7 +18,7 @@ const Student = ({student, toUpdate, toDelete}) => {
             <Card.Body>
             <Card.Title>{student.firstname} {student.lastname}</Card.Title>
             <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
-            <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoSync/></Button>
+            <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}> <ioicons.IoSync/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoEye/></Button>
             </Card.Body>
         </Card>
