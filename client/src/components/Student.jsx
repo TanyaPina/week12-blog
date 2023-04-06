80c872587fddf6a68f87de8f01eea58ea5464eee
Tanya Pina <tanyapina15@gmail.com>
2023-04-06 16:13:31 -0700

    imported elements from material ui

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import * as ioicons from 'react-icons/io5'
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
-const Student = ({student, toUpdate, toDelete}) => {
+const Student = ({student, toUpdate, toDelete, toSee}) => {
 
     const onUpdate = (toUpdateStudent) => {
         toUpdate(toUpdateStudent)
@@ -12,11 +16,13 @@ const Student = ({student, toUpdate, toDelete}) => {
     const onDelete = (toDeleteStudent) => {
         toDelete(toDeleteStudent)
     }
-
+    
     return (
         <Card>
             <Card.Body>
-            <Card.Title>{student.firstname} {student.lastname}</Card.Title>
+            <Card.Title>{student.firstname} {student.lastname} </Card.Title>
+            {/*<img src={student.url} alt={student.firstname}/>*/}
+            {/*<img src="https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="BigCo Inc. logo"/>*/}
             <Button variant="outline-danger" onClick={()=>{onDelete(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}><ioicons.IoTrash/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em', marginRight:'0.9em'}}> <ioicons.IoSync/></Button>
             <Button variant="outline-info" onClick={()=>{onUpdate(student)}} style={{padding: '0.6em'}}> <ioicons.IoEye/></Button>
