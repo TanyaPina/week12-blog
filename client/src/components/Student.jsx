712a74df3dce15d0d96776b5203f858b6f45ba46
Tanya Pina <tanyapina15@gmail.com>
2023-04-07 10:20:27 -0700

    passed props to modal title

--- client/src/components/Student.jsx
+++ client/src/components/Student.jsx
@@ -50,7 +50,7 @@ const Student = ({ student, toUpdate, toDelete, toSee }) => {
 >
   <Box sx={style}>
     <Typography id="modal-modal-title" variant="h6" component="h2">
-      Text in a modal
+    {student.firstname} {student.lastname}
     </Typography>
     <Typography id="modal-modal-description" sx={{ mt: 2 }}>
       Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
